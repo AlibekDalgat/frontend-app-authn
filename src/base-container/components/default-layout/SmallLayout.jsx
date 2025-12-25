9bfec9b0cbc84e16075b3762f4a32d6046245cb3
AlibekDalgat <johnbuck_kebarty@mail.ru>
2025-12-25 13:00:14 +0300

    Вид шапки страницы с телефона

--- src/base-container/components/default-layout/SmallLayout.jsx
+++ src/base-container/components/default-layout/SmallLayout.jsx
@@ -11,22 +11,20 @@ const SmallLayout = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <span className="bg-primary-400 w-100">
-      <div className="col-md-12 small-screen-top-stripe" />
+    <span className="custom-light-background w-100">
       <div>
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-          <Image className="logo-small" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
         </Hyperlink>
         <div className="d-flex align-items-center m-3.5">
           <div className={classNames({ 'small-yellow-line mr-n2.5': getConfig().SITE_NAME === 'edX' })} />
           <h1
             className={classNames(
-              'text-white mt-3.5 mb-3.5',
+              'text-custom mt-3.5 mb-3.5',
             )}
           >
             <span>
               {formatMessage(messages['start.learning'])}{' '}
-              <span className="text-accent-a d-inline-block">
+              <span className="text-custom d-inline-block">
                 {formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME })}
               </span>
             </span>
